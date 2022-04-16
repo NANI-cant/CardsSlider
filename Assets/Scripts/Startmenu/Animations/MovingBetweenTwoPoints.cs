869b411d158fba5ca3ae662500945e349f45d5f9
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2022-04-17 01:43:41 +0300

    correct figures movements and working on settings menu

--- Assets/Scripts/Startmenu/Animations/MovingBetweenTwoPoints.cs
+++ Assets/Scripts/Startmenu/Animations/MovingBetweenTwoPoints.cs
@@ -6,32 +6,59 @@ public class MovingBetweenTwoPoints : MonoBehaviour {
 
     [SerializeField] private Vector2 _endPointFormal;
 
+    private RectTransform _rectTransform;
     private Vector2 _startPoint;
     private float _durationAnimation = 0.5f;
     private Camera _camera;
-    private RectTransform _rectTransform;
+    private Vector2 _endPoint;
+
+    private float _calculatedNewHeight;
+    private float _calculatedNewWidth;
 
     [Inject]
     public void Construct(Camera camera){
         _camera = camera;
-        
     }
 
     private void Awake() {
         _rectTransform = GetComponent<RectTransform>();
+        
     }
 
     private void Start() {
-        _startPoint = transform.position;
+        _startPoint = _rectTransform.localPosition;
+        Calculate();
         MoveToOtherPoint();
+        // MoveToAnotherPoint();
+    }
+
+    private void Update(){
+        Calculate();
+        MoveToOtherPoint();
+        //MoveToAnotherPoint();
+    }
+
+    private void Calculate(){
+        float heightFactor = _camera.pixelHeight / 1920f;
+        float widthFactor = _camera.pixelWidth / 1080f;
+        float averageFactor = (heightFactor + widthFactor) / 2f;
+        _calculatedNewHeight = _camera.pixelHeight / averageFactor;
+        _calculatedNewHeight = _calculatedNewHeight / 2f;
+
+        _calculatedNewWidth = _camera.pixelWidth / averageFactor;
+        _calculatedNewWidth = _calculatedNewWidth / 2f;
     }
 
     private void MoveToOtherPoint() {
-        Vector2 _endPoint;
-        _endPoint.x = _camera.pixelWidth/2 * _endPointFormal.x;
-        _endPoint.y = _camera.pixelHeight/2 * _endPointFormal.y;
+        _endPoint.x = _calculatedNewWidth * _endPointFormal.x;
+        _endPoint.y = _calculatedNewHeight * _endPointFormal.y;
+        //Debug.Log(_endPoint);
         _rectTransform.DOAnchorPos(_endPoint, _durationAnimation);
-        // _rectTransform.DOMove(_endPoint, _durationAnimation);
+       
+    }
+
+    private void MoveToAnotherPoint(){
+        _rectTransform.DOAnchorPos(_startPoint, _durationAnimation);
     }
 
     // private void OnDrawGizmosSelected() {
