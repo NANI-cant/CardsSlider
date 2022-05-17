79a40121d2b7f3e4728b59b8d828235eb6f95ac8
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2022-05-17 16:40:15 +0300

    fix base on comments
    fix scripts of the buttons, fix scripts of the figures

---
+++ Assets/Scripts/Startmenu/ButtonHandler/ShopMenuButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopMenuButton : MonoBehaviour
+{
+    [SerializeField] private MovableFigures[] figures;
+    private Button buttonMenuSections;
+
+    private void Awake(){
+        buttonMenuSections = GetComponent<Button>();
+    }
+
+    private void OnEnable(){
+        buttonMenuSections.onClick.AddListener(MoveFiguresToBorder);
+    }
+
+    private void OnDisable(){
+        buttonMenuSections.onClick.RemoveListener(MoveFiguresToBorder);
+    }
+
+    public void MoveFiguresToBorder(){
+        foreach(MovableFigures figure in figures){
+            figure.MoveOutOfBorder();
+        }
+    }
+}
