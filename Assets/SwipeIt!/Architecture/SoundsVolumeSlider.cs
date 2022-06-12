fad4277bf6f5adfc9ccf47767b49622b177e7b1f
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2022-06-12 19:10:46 +0300

    all svg shapes are distributed in folders, and volume control sliders are connected

---
+++ Assets/SwipeIt!/Architecture/SoundsVolumeSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+[RequireComponent(typeof(Slider))]
+public class SoundsVolumeSlider : MonoBehaviour
+{
+    private Slider _slider;
+    private GameSettings _gameSettings;
+
+    private void Awake(){
+        _slider = GetComponent<Slider>(); 
+        _slider.value = _gameSettings.SoundsVolume;
+    }
+
+    [Inject]
+    public void Construct(GameSettings gameSettings){
+        _gameSettings = gameSettings;
+    }
+
+    private void OnEnable(){
+        _slider.onValueChanged.AddListener(OnValueSliderChanged);
+    }
+
+    private void OnDisable(){
+        _slider.onValueChanged.RemoveListener(OnValueSliderChanged);
+    }
+
+    private void OnValueSliderChanged(float value){
+        _gameSettings.SoundsVolume = value;
+    }
+}
