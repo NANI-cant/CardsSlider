41de1152f348e64de56316f60d0582f01759409d
NANI <trixxx.asd@yandex.ru>
2022-07-11 19:25:04 +0300

    timer shaking

--- Assets/SwipeIt!/Scenes/Classic/UI/SceneEntryAnimation.cs
+++ Assets/SwipeIt!/Scenes/Classic/UI/SceneEntryAnimation.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class SceneEntryAnimation : MonoBehaviour {
     [Header("Layouts")]
-    [SerializeField] private RectTransform _targetFigure;
     [SerializeField] private RectTransform _timer;
     [SerializeField] private RectTransform _lifes;
     [SerializeField] private RectTransform _score;
@@ -21,7 +20,6 @@ public class SceneEntryAnimation : MonoBehaviour {
     }
 
     private void Prepare(){
-        _targetFigure.localScale = _startScale;
         _timer.localScale = _startScale;
         _lifes.localScale = _startScale;
         _score.localScale = _startScale;
@@ -33,7 +31,6 @@ public class SceneEntryAnimation : MonoBehaviour {
         _score.DOScale(_endScale, _durationAnimation);
         _lifes.DOScale(_endScale, _durationAnimation);
         _pauseButton.DOScale(_endScale, _durationAnimation);
-        _targetFigure.DOScale(_endScale, _durationAnimation);
         _timer.DOScale(_endScale, _durationAnimation);
         _hud.DOFade(1, _durationAnimation);
     }
