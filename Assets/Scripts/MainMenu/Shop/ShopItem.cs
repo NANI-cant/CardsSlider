84b8514998d793126ec3dfe1f99d3c21d55f3ff8
NANI <57311180+NANI-cant@users.noreply.github.com>
2022-10-02 16:05:57 +0300

    New figure collection (#26)
    * add new collection
    
    * Fix target figure bug
    
    * Add shop item conditions
    
    * Update ShopItem.prefab

--- Assets/Scripts/MainMenu/Shop/ShopItem.cs
+++ Assets/Scripts/MainMenu/Shop/ShopItem.cs
@@ -1,22 +1,38 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿using System;
+using UnityEngine;
 
 namespace StartMenu {
+    public enum ShopItemCondition {
+        Buyable,
+        NotEnoughMoney,
+        UnSelected,
+        Selected,
+    }
+
     public class ShopItem : MonoBehaviour, IBuyable, ISelectable {
-        public UnityAction ConditionChanged;
+        public event Action<ShopItemCondition> ConditionChanged;
 
         private ItemData _itemData;
         private Bank _bank;
-        private FigureCollectionsHolder _banksHolder;
+        private FigureCollectionsHolder _collectionsHolder;
         private PlayerProgress _progress;
+        private ShopItemCondition _condition;
 
         public ItemData ItemData => _itemData;
 
         public void Construct(Bank bank, FigureCollectionsHolder figureCollectionsHolder, PlayerProgress progress, ItemData itemData) {
             _bank = bank;
-            _banksHolder = figureCollectionsHolder;
+            _collectionsHolder = figureCollectionsHolder;
             _progress = progress;
             _itemData = itemData;
+        }
+
+        private void OnEnable() {
+            _collectionsHolder.CollectionChanged += OnCollectionChanged;
+        }
+
+        private void OnDisable() {
+            _collectionsHolder.CollectionChanged -= OnCollectionChanged;
         }
 
         private void Start() {
@@ -24,7 +40,28 @@ namespace StartMenu {
                 _progress.SetShopItemAvailable(_itemData);
             }
             _itemData.IsAvailable = _progress.IsShopItemAvailable(_itemData);
-            ConditionChanged?.Invoke();
+
+            SetStartCondition();
+            ConditionChanged?.Invoke(_condition);
+        }
+
+        private void SetStartCondition() {
+            if (_itemData.IsAvailable) {
+                if (_collectionsHolder.SelectedCollection == _itemData.FiguresCollection) {
+                    _condition = ShopItemCondition.Selected;
+                    return;
+                }
+
+                _condition = ShopItemCondition.UnSelected;
+                return;
+            }
+
+            if (_bank.Amount < _itemData.BasePrice) {
+                _condition = ShopItemCondition.NotEnoughMoney;
+                return;
+            }
+
+            _condition = ShopItemCondition.Buyable;
         }
 
         public void Buy() {
@@ -34,17 +71,26 @@ namespace StartMenu {
             _progress.SetShopItemAvailable(_itemData);
             _itemData.IsAvailable = true;
 
-            ConditionChanged?.Invoke();
+            _condition = ShopItemCondition.UnSelected;
+            ConditionChanged?.Invoke(_condition);
         }
 
         public void Select() {
-            _banksHolder.SelectBank(_itemData.FiguresCollection);
+            _collectionsHolder.SelectCollection(_itemData.FiguresCollection);
+
+            _condition = ShopItemCondition.Selected;
+            ConditionChanged?.Invoke(_condition);
         }
 
         private bool CheckCanBeBuyedFor(int amount) {
             if (_itemData.IsAvailable) return false;
             if (amount >= _itemData.BasePrice) return true;
             return false;
+        }
+
+        private void OnCollectionChanged(FiguresCollection obj) {
+            if (_condition == ShopItemCondition.Selected) _condition = ShopItemCondition.UnSelected;
+            ConditionChanged?.Invoke(_condition);
         }
     }
 }
